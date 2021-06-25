b1d0c6f7d60d9494568044edf55fe73c7a030fbd
chronolai <x941i6c04@gmail.com>
2021-06-25 23:17:00 +0800

    test baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'English Summer',
   tagline: 'Dinosaurs are cool',
   url: 'https://chronolai.github.io',
-  baseUrl: '/english-summer/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/abc.png',
