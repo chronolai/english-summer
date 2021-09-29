85d26f29adc506e3008404918e9b83745c9344db
gh-actions <x941i6c04@gmail.com>
2021-09-29 13:30:03 +0000

    Deploy website - based on 9b2fa8f6a501f71a3806ebfd8a5bd10c01bb52df

---
+++ assets/js/e9489857.47651d11.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktai=self.webpackChunktai||[]).push([[2064],{5604:function(e){e.exports=JSON.parse('{"permalink":"/english-summer/blog","page":1,"postsPerPage":10,"totalPages":8,"totalCount":76,"previousPage":null,"nextPage":"/english-summer/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
